64768770c15af306ef3474dd011a72700c27a237
y0014984 <y0014984@gmail.com>
2024-12-14 18:01:09 +0100

    rewrite of keyboard input

--- main.6502.asm
+++ main.6502.asm
@@ -36,11 +36,48 @@ CURRENT_KEYPRESS    = $8003
 
     //JSR TEST_SCREEN
 
+    JMP BIOS_START
+
+// ========================================
+
+BIOS_START
     LDA #$81                        // unused ASCII code is now Cursor
     JSR PRINT_CHAR
+@loop
+    JSR GET_CHAR_FROM_BUFFER
+    CMP #$00
+    BEQ @loop
+    CMP #$08                        // BACKSPACE
+    BEQ @backspace
+    CMP #$0A                        // LINE FEED
+    BEQ @enter
+    JSR PRINT_CHAR
+    JSR INCREMENT_CURSOR
+@printCursor
+    LDA #$81                        // unused ASCII code is now Cursor
+    JSR PRINT_CHAR
+    JMP @loop
+@backspace
+    LDA #$20                        // SPACE
+    JSR PRINT_CHAR                  // override current pos with blank to clear cursor
+    JSR DECREMENT_CURSOR
+    JMP @printCursor
+@enter
+    LDA #$20                        // SPACE
+    JSR PRINT_CHAR                  // override current pos with blank to clear cursor
+    LDA #0                          // set cursor to next line
+    STA CUR_POS_X                   // by setting x to 0
+    INC CUR_POS_Y                   // and increasing y until
+    LDA CUR_POS_Y                   // height reached
+    CMP #SCREEN_HEIGHT              // then return to first line
+    BNE @printCursor
+    LDA #0
+    STA CUR_POS_Y
+    JMP @printCursor
 
-PROGRAM_LOOP                        // main Bios loop
-    JMP PROGRAM_LOOP
+// ========================================
+
+EDITOR
 
 // ========================================
 
@@ -498,11 +535,11 @@ READ_KEYBOARD                       // check all bits of $0200 - $0209 (hardware
     LDA #$00
     JMP @continue
 @Enter
-    LDA #$0A                            // line feed
-    JMP @continueEnter
+    LDA #$0A                            // LINE FEED
+    JMP @continue
 @Backspace
-    LDA #$20
-    JMP @continueBackspace
+    LDA #$08                            // BACKSPACE
+    JMP @continue
 @Escape
     LDA #$00
     JMP @continue
@@ -588,34 +625,6 @@ READ_KEYBOARD                       // check all bits of $0200 - $0209 (hardware
     STA LAST_KEYPRESS
     JMP @return
 
-@continueEnter
-    STA CURRENT_KEYPRESS                        // a contains keypress
-    CMP LAST_KEYPRESS                           // if previous key is still pressed do nothing
-    BEQ @return
-    STA LAST_KEYPRESS                           // store current keypress in last keypress
-    LDA #$20                                    // ASCII blank space
-    JSR PRINT_CHAR                              // override current pos with blank to clear cursor
-    LDA #0                                      // set cursor to next line
-    STA CUR_POS_X                               // by setting x to 0
-    INC CUR_POS_Y                               // and increasing y until
-    LDA CUR_POS_Y                               // height reached
-    CMP #SCREEN_HEIGHT                          // then return to first line
-    BNE @printCursor
-    LDA #0
-    STA CUR_POS_Y
-    JMP @printCursor
-
-@continueBackspace
-    STA CURRENT_KEYPRESS                        // a contains keypress
-    CMP LAST_KEYPRESS                           // if previous key is still pressed do nothing
-    BEQ @return
-    STA LAST_KEYPRESS                           // store current keypress in last keypress
-    JSR PRINT_CHAR                              // override current pos with blank to clear cursor
-    JSR DECREMENT_POINTER
-    JSR PRINT_CHAR                              // print keypress to screen
-    LDA CURRENT_KEYPRESS
-    JMP @printCursor
-
 @continue
     CMP #$41                                    // check if key is a shiftable character 
     BCC @upperCase                              // if >=$41 and <=$5A
@@ -633,13 +642,58 @@ READ_KEYBOARD                       // check all bits of $0200 - $0209 (hardware
     CMP LAST_KEYPRESS                           // if previous key is still pressed do nothing
     BEQ @return
     STA LAST_KEYPRESS                           // store current keypress in last keypress
-    JSR PRINT_CHAR                              // print keypress to screen
-    JSR INCREMENT_POINTER
+    CMP #$00
+    BEQ @return
+    JSR ADD_CHAR_TO_BUFFER
 
-@printCursor
-    LDA #$81                                    // unused ASCII code is now Cursor
-    JSR PRINT_CHAR
+@return
+    RTS
 
+// ========================================
+
+keyboardBuffer .storage 16, $00                 // 16 bytes of keyboardBuffer
+keyboardBufferPos .byte $00
+
+// ========================================
+
+ADD_CHAR_TO_BUFFER
+/*     BRK
+    NOP */
+    LDX keyboardBufferPos                       // store char in keyboard buffer
+    STA keyboardBuffer,X
+    INC keyboardBufferPos                       // check for buffer overrun
+    LDA keyboardBufferPos
+    CMP #16
+    BNE @return
+@reset                                          // buffer overrun
+    LDA #0
+    STA keyboardBufferPos
+@return
+    RTS
+
+// ========================================
+
+GET_CHAR_FROM_BUFFER
+    LDX keyboardBufferPos                       // if buffer is empty return $00 byte
+    CPX #0
+    BEQ @bufferEmpty
+    LDA keyboardBuffer                          // load 1st char from keyboard input buffer
+@shrinkBuffer
+    PHA                                         // push A to stack
+    LDX #0                                      // remove char from input buffer by
+    LDY #1                                      // shifting all chars one byte to the left
+@loop
+    LDA keyboardBuffer,Y
+    STA keyboardBuffer,X
+    INX
+    INY
+    CPX keyboardBufferPos
+    BNE @loop
+    DEC keyboardBufferPos                       // reduce buffer size by one
+    PLA                                         // pull A from stack
+    JMP @return
+@bufferEmpty
+    LDA #$00
 @return
     RTS
 
@@ -702,7 +756,7 @@ PRINT_CHAR                                      // print char stored in A to scr
 
 // ========================================
 
-INCREMENT_POINTER
+INCREMENT_CURSOR
 @incX
     INC CUR_POS_X                               // set cur pos to next pos
     LDX CUR_POS_X
@@ -725,7 +779,7 @@ INCREMENT_POINTER
 
 // ========================================
 
-DECREMENT_POINTER
+DECREMENT_CURSOR
 @decX
     DEC CUR_POS_X                               // set cur pos to previous pos
     LDX CUR_POS_X
